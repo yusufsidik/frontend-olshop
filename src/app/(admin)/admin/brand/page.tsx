ae8a898db061707b53da316ce5f6d6fd34629c4e
yusufsidik <yusufsidik36@gmail.com>
2025-03-21 16:16:08 +0800

    update page category

--- src/app/(admin)/admin/brand/page.tsx
+++ src/app/(admin)/admin/brand/page.tsx
@@ -6,53 +6,7 @@ import { Plus } from 'lucide-react';
 
 import { Suspense } from "react";
 
-interface Blog {
-  id: number;
-  title: string;
-  content: string;
-  author: string; 
-  date: string;
-  category: string;
-}
-
-function tanggal(date: string) {
-  const tanggal = new Date(date).toLocaleString('id-ID', {
-    day: '2-digit',
-    month: '2-digit',
-    year: 'numeric',  
-  }).toString().split('/').reverse().join('-');
-  return tanggal
-}
-
-
-export default async function Brand() {
-
-  const data = await fetch('https://api.vercel.app/blog')
-  const blogs : Blog[] = await data.json()
-  
-
-  const brands = [
-    {
-      id: 1,
-      name: "Asus"
-    },
-    {
-      id: 2,
-      name: "Lenovo"
-    },
-    {
-      id: 3,
-      name: "Acer"
-    },
-    {
-      id: 4,
-      name: "Dell"
-    },
-    {
-      id: 5,
-      name: "Axio"
-    }
-  ]
+export default function Brand() {
 
   return (
     <Card className="lg:col-span-4">
@@ -73,49 +27,22 @@ export default async function Brand() {
           <TableHeader>
             <TableRow>
               <TableHead>No</TableHead>
-              <TableHead>Brand Name</TableHead>
-              <TableHead>Action</TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            {brands.map((brand, index) => (
-              <TableRow key={brand.id}>
-                <TableCell className="font-medium">{index + 1}</TableCell>
-                <TableCell>{brand.name}</TableCell>
-                <TableCell className="flex gap-x-2">
-                  <Button variant="secondary">Edit</Button>
-                  <Button variant="destructive">Delete</Button>
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table> */}
-        <Table>
-          <TableHeader>
-            <TableRow>
-              <TableHead>ID</TableHead>
-              <TableHead>Title</TableHead>
-              <TableHead>Content</TableHead>
-              <TableHead>Author</TableHead>
-              <TableHead>Date</TableHead>
-              <TableHead>Category</TableHead>
+              <TableHead>Name</TableHead>
+              <TableHead>Categories</TableHead>
             </TableRow>
           </TableHeader>
           <Suspense fallback={<div>Loading...</div>}>
             <TableBody>
-              {blogs.map((blog, index) => (
-                <TableRow key={blog.id}>
+              {subcategories.map((category, index) => (
+                <TableRow key={category._id}>
                   <TableCell className="font-medium">{index + 1}</TableCell>
-                  <TableCell>{blog.title}</TableCell>
-                  <TableCell>{blog.content}</TableCell>
-                  <TableCell>{blog.author}</TableCell>
-                  <TableCell>{blog.date}</TableCell>
-                  <TableCell>{blog.category}</TableCell>
+                  <TableCell>{category.name}</TableCell>
+                  <TableCell>{category.subcategories.map((subcat, index) => <p key={index}>{subcat}</p>)}</TableCell>
                 </TableRow>
               ))}
             </TableBody>
           </Suspense>
-        </Table>
+        </Table> */}
       </CardContent>
       <CardFooter className="flex justify-between">
         <Button variant="outline">Previous</Button>
